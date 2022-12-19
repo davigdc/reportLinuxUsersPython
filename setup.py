05adcbd027f4ab2758739f114a589e175f8ee7b6
davigdc <davigdc10@gmail.com>
2022-12-19 20:58:54 -0300

    estruturando cli

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open ('README.md', encoding='UTF-8') as f:
+    readme = f.read()
+
+setup(
+    name='hr',
+    version='1.0.0',
+    description='Command line user export utility',
+    long_description=readme,
+    author='Davi Carmo',
+    author_email='davigdc10@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[]
+)
